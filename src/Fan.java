e471540b4be598b22b973bd6205ca750c326e431
thaihung198 <thaihung8503@gmail.com>
2025-04-24 09:43:49 +0700

    first commit

---
+++ src/Fan.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Fan {
+    public static final int SLOW = 1;
+    public static final int MEDIUM = 2;
+    public static final int FAST = 3;
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    public Fan() {
+    }
+
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        if(this.on){
+            return "Fan{speed=" + speed + ", radius=" + radius + ", color=" + color + "- fan is on" + "}";
+        } else {
+            return "Fan{ radius=" + radius + ", color=" + color + "- fan is off" + "}";
+        }
+    }
+
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        fan1.setSpeed(Fan.FAST);
+        fan1.setRadius(10.0);
+        fan1.setColor("yellow ");
+        fan1.setOn(true);
+        System.out.println("Fan 1  " + fan1.toString());
+
+        Fan fan2 = new Fan();
+        fan2.setSpeed(Fan.MEDIUM);
+        fan2.setRadius(10.0);
+        fan2.setColor("blue");
+        fan2.setOn(false);
+        System.out.println("Fan 2: " + fan2.toString());
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+
+    }
+}
